3390f0486f849cb560c1c63d56e1cd120b1e372c
Weston C <wgithub@canncentral.org>
2010-03-30 01:21:56 -0700

    PDOE.class.php now has and passes rudimentary tests README now actually has content

--- PDOE.class.php
+++ PDOE.class.php
@@ -3,21 +3,70 @@
 //include_once("MSGS.class.php");
 
 class PDOE extends PDO {
+		
+	var $driver;
 
-	/* static $qcache = array();
-
-	function prepare($queryStr) {
-		if(isset(self::$qcache[$queryStr]))
-			return self::$qcache[$queryStr];
-		else
-			return parent::prepare($queryStr);
-	} */
+	function __construct($dsn,$user=null,$pass=null,$options=null) {
+		parent::__construct($dsn,$user,$pass,$options);
+		$this->driver = substr($dsn,0,strpos($dsn,':'));
+	}
 
 	function operate($args) {
 		extract($args);
-		$fetchOpts = isset($fetchOpts) ? $fetchOpts : null;
+		/* potential arguments:
+			sql:
+
+			table:
+			limit:	
+			sort:
+			where:	
+			cols:
+
+			o:
+			m:
+
+			mapWith:
+			reduceBy:
+			f:
+		 */
+		
+		$fetchOpts = isset($fetchOpts) ? $fetchOpts : PDO::FETCH_ASSOC;
+		if( isset($table) && !isset($sql) ) {
+			$cols = isset($cols) ? 
+				(is_array($cols) ? implode(', ',$cols) : $cols) :
+				'*';
+			if(isset($limit)) {
+				$limit = "limit $limit";
+				$offset = isset($offset) ? $offset :
+					(isset($page) ? "offset ".($page*$limit) : null);
+			} else 
+				$limit = null;
+
+			if(isset($sort)) {
+				if(is_array($sort)) {
+					$orderBy = implode(', ',$sort);
+					$orderBy = "order by $orderBy";
+				} else if(is_string($sort)) 
+					$orderBy = "order by $sort";
+				else 
+					$orderBy = null;
+			} else
+				$orderBy = null;
+		
+			if( isset($where) ) {
+				if(is_array($where)) {
+					$tmp = $this->array2where($where);
+					$where = $tmp['where'];
+					$params = $tmp['params'];
+				} 
+			} else
+				$where = null;
+
+			$sql = "select $cols from $table $where $limit $offset $orderBy";
+		}
 		if( isset($sql) ) 
 			$sth = $this->prepare($sql);
+			
 
 		if(!isset($sth)) {
 			throw new Exception("No statement to operate over");
@@ -57,23 +106,23 @@ class PDOE extends PDO {
 		return $rv;
 	}
 
-	function saverec($rec,$table,$identifier=null) {
-		$this->_msg("CALL: PDOE::saverec($rec,$table,$identifier)");
+	function saverec($table,$rec,$identifier=null) {
+		$this->_msg("CALL: PDOE::saverec($table,$rec,$identifier)");
 		if(is_array($identifier) && (count(identifier) > 0)) { 
-			$rv = $this->updaterec($rec,$table,$identifier);
+			$rv = $this->updaterec($table,$rec,$identifier) ? true: false;
 		} 
 		else if(($prikey = $this->prikey($table)) && isset($rec[$prikey])) {
-			$rv = $this->updaterec($rec,$table,array($prikey=>$rec[$prikey]));
+			$rv = $this->updaterec($table,$rec,array($prikey=>$rec[$prikey])) ? true : false;
 		}
 		else {
-			$rv = $this->insertrec($rec,$table);
+			$rv = $this->insertrec($table,$rec);
 		}
-		$this->_msg("RETURN: PDOE::saverec($rec,$table,$identifier) = $rv");
+		$this->_msg("RETURN: PDOE::saverec() = $rv");
 		return $rv;
 	}
 
-	function updaterec($rec,$table,$where=null) {
-		$this->_msg("CALL: PDOE::updaterec($rec,$table,$where)");
+	function updaterec($table,$rec,$where=null) {
+		$this->_msg("CALL: PDOE::updaterec($table,$rec,$where)");
 		// possibly query could be hashed by table and where parameters
 		// to an already prepared statement
 		$query = array(
@@ -86,80 +135,190 @@ class PDOE extends PDO {
 			$comma = ', ';	
 			$values[] = $v;
 		}
-		$query[] = 	' WHERE ';
 		if( is_array($where) && (($whereTermCount = count($where)) > 0) )  {
 			$this->_msg("Update via specific where parameters");
-			$comma = ' AND ';
+			$query[] = 	' WHERE ';
+			$comma = '';
 			foreach($where as $k => $v) {
 				array_push($query,$comma,$k,'=?');
 				$values[] = $v;
+				$comma = ' AND ';
 			}
 		} else if($prikey = PDOE::prikey($table)) {
 			$this->_msg("update via implicity primary key");
+			$query[] = 	' WHERE ';
 			if(isset($rec[$prikey])) {
 				array_push($query,' ',$prikey,'=',$rec[$prikey]);
 			} else {
 				$this->_msg("except we can't find the primary key. Hmm.");
 				$rv = $result ? $rec[$prikey] : $result;
 			}
-		}
+		} else
+			throw new Exception("no WHERE or implicit key given");
 		$query = implode(null,$query);
 		$this->_msg("QUERY: $query");
-		$sth = $this->prepare($query);
-		$result = $sth->execute($values);
+		if($sth = $this->prepare($query))
+			$result = $sth->execute($values);
+		else
+			throw new Exception("query wasn't succesfully prepared |$query|");
+			//$result = false;
 		$rv = $result ? $sth->rowCount() : false;
 		$this->_msg("RETURN: PDOE::updaterec() = $rv");
 		return $rv;
 	}
 
-	function insertrec($rec,$table) {
-		$this->_msg("CALL: PDOE::insertrec($rec,$table)");
-		$keys = array_keys($rec);
+	function insertrec($table,$rec) {
+		$this->_msg("CALL: PDOE::insertrec($table,$rec)");
 		$placeholders = array_fill(0,count($rec),'?');
 		$values = array_values($rec);
-		// possibly query could be hashed by table 
-		// to an already prepared statement
-		$query = implode(null,array(
-			'INSERT INTO ',$table,' (',
-			implode(', ',$keys),') VALUES (',
-			implode(', ',$placeholders),') '
-		));
-		$sth = $this->prepare($query);	
+		if(!array_key_exists(0,$rec) && ($keys = array_keys($rec)) ) {
+			$query = implode(null,array(
+				'INSERT INTO ',$table,' (',
+				implode(', ',$keys),') VALUES (',
+				implode(', ',$placeholders),') '
+			));
+		} else {
+			$query = implode(null,array(
+				'INSERT INTO ',$table,' VALUES (',
+				implode(', ',$placeholders),') '
+			));
+		}
+		if(!$sth = $this->prepare($query))
+			throw new Exception("failure preparing query |$query| - no statement handle");
 		$result = $sth->execute($values);
 		$rv = $result ? $this->lastInsertId() : false;
 		$this->_msg("RETURN: PDOE::insertrec() = $rv");
 		return $rv;
 	}	
 
+	function fetch($table,$where=null) {
+		$this->_msg("CALL: PDOE::fetch($table,$where)");
+		// possibly query could be hashed by table and where parameters
+		// to an already prepared statement
+		$query = array(
+			"SELECT * from $table",
+		);
+		if( is_array($where) ) {
+			$this->_msg("Select via specific where parameters");
+			$query[] = 	' WHERE ';
+			$comma = '';
+			foreach($where as $k => $v) {
+				array_push($query,$comma,$k,'=?');
+				$values[] = $v;
+				$comma = ' AND ';
+			}
+		} else if($prikey = PDOE::prikey($table)) {
+			if(!empty($where)) {
+				$query[] = 	" WHERE $prikey=?";
+				$values = array($where);
+			} else {
+				$query[] = 	" ORDER BY $prikey DESC";
+				$values = array();
+			}
+			$this->_msg("select by primary key");
+		} 
+		$query[] = " LIMIT 1;";
+		$query = implode(null,$query);
+		$this->_msg("QUERY: $query");
+		if( ($sth = $this->prepare($query)) 
+		 && !($sth->execute($values) === false) ) {
+		 	$rv = $sth->fetch(PDO::FETCH_ASSOC);
+		} else 
+		 	$rv = false;
+		$this->_msg("RETURN: PDOE::fetch() = $rv");
+		return $rv;
+	}
+
+	function deleterec($table,$where=null) {
+		if(is_array($where)) {
+			list($wheresql,$params) = $this->array2where($where);
+			$query = "DELETE FROM $table WHERE $wheresql";
+		} else if(!empty($where) && $prikey = PDOE::prikey($table)) {
+			$query = "DELETE FROM $table WHERE $prikey=?";
+			$params = array($where);
+		} else
+			throw new Exception("won't delete without selection");
+		if($sth = $this->prepare($query))
+			$rv = $sth->execute($params);
+		else
+			throw new Exception("couldn't prepare query |$query|");
+		return $rv;
+	}
+
 	function prikey($table,$forcecheck=false) {
 		global $_prikey_cache;
-		if(!$forcecheck && isset($_prikey_cache[$table])) {
+		if( !$forcecheck && isset($_prikey_cache[$table]) ) {
 			$rv = $_prikey_cache[$table];
-		} else {
-			$sth = $this->prepare("describe $table");
-			$rv = null;
-			while($row = mysql_fetch_array($result,MYSQL_BOTH)) {
-				if($row['Key'] == 'PRI') {
-					$rv = $row['Field'];
-					$_prikey_cache[$table] = $rv;
-					break;
-				}
+		} else if($this->driver == 'sqlite') {
+			$rv = $this->_prikey_sqlite($table);
+		} else
+			$rv = $this->_prikey_mysql($table);
+		return $rv;
+	}
+
+	function _prikey_sqlite($table,$forcecheck=false) {
+		$sth = $this->query("pragma table_info ($table)");
+		$rv = null;
+		if($sth)
+		while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+			if($row['pk'] == 1) {
+				$rv = $row['name'];
+				$_prikey_cache[$table] = $rv;
+				break;
 			}
 		}
 		return $rv;
 	}
 
+	function _prikey_mysql($table) {
+		$sth = $this->query("describe $table");
+		$rv = null;
+		if($sth)
+		while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+			if($row['Key'] == 'PRI') {
+				$rv = $row['Field'];
+				$_prikey_cache[$table] = $rv;
+				break;
+			}
+		}
+		return $rv;
+	}
+
+	function array2where($a) {
+		$sql = array();
+		$values = array();
+		foreach($a as $k => $v) {
+			if(is_array($v)) {
+				array_merge($values,$v);
+				array_push($sql,'(',$k,' IN (',implode(',',$v),'))');
+			} else {
+				array_push($values,$v);
+				array_push($sql,'(',$k,'=?)');
+			}
+		}
+		if(count($sql) > 0) {
+			$sql = implode(' AND ',$sql);
+			$sql = "WHERE $sql";
+		} else
+			$sql = '';
+		return array('sql' => $sql, 'values' => $values);
+	}
+
 	function _msg($m) {
+		$this->_msgs[] = $m;
 		//MSGS::add($m);
 	}
 
 	function QueryCollector() {
-		return new PDOE_QueryCollector();
+		return new PDOE_QC();
 	}
-	
+
+	function QC() {
+		return new PDOE_QC();
+	}
 }
 
-class PDOE_QueryCollector {
+class PDOE_QC {
 
 	var $rows;
 
@@ -191,7 +350,7 @@ class PDOE_QueryCollector {
 	}
 
 	function __toString() {
-		return "Object (PDOE_QCollector)";
+		return "Object (PDOE_QC) (a *Q*uery *C*ollector)";
 	}
 }
 
